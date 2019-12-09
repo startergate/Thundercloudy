c8c1610d14c4c80f954662b1a6a692c4ebab352c
STARTERGATE <32726809+startergate@users.noreply.github.com>
2019-12-09 23:41:51 +0900

    [UPDATE] Signup Button
    From Now On, THE Sign up button will send you to the internet

--- app/src/main/java/com/startergate/thundercloudy/MainActivity.java
+++ app/src/main/java/com/startergate/thundercloudy/MainActivity.java
@@ -1,17 +1,29 @@
 package com.startergate.thundercloudy;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-
+    Button signup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        signup = findViewById(R.id.signup);
 
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("http://sid.donote.co:3000/user/login")));
+            }
+        });
     }
 }
