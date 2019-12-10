823abb62c525e1625514f477ef20fe15796647d6
STARTERGATE <32726809+startergate@users.noreply.github.com>
2019-12-10 21:21:26 +0900

    [UPDATE] Activate ImageButton on Sns

--- app/src/main/java/com/startergate/thundercloudy/SnsActivity.java
+++ app/src/main/java/com/startergate/thundercloudy/SnsActivity.java
@@ -8,9 +8,17 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class SnsActivity extends AppCompatActivity {
 
+    ImageButton btn_main, btn_add, btn_cloud;
+
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sns);
+
+        btn_main = findViewById(R.id.btn_main);
+        btn_add = findViewById(R.id.btn_add);
+        btn_cloud = findViewById(R.id.btn_cloud);
+
+        btn_main.setBackgroundResource(R.drawable.border_activated);
     }
 }
